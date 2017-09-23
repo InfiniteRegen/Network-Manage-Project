0f6e06b60dbe4383176d15408eae35b00a71dadc
DanielLee <31802525+InfiniteRegen@users.noreply.github.com>
2017-09-23 21:19:11 +0900

    Update capture.c

--- capture.c
+++ capture.c
@@ -1,62 +1,57 @@
 #include "capture.h"
 
+void DieWithError(char *errMsg)
+{
+  printf("usage: %s [-f] [pktFileName.pkt] [-t] [number of max packet]\n", argv[0]);
+  perror(errMsg);
+  exit(-1);
+}
+
 int main(int argc, char** argv)
 {
 	/* etc familiy */
-	bool fileOption=false;
+	bool fileOption = false; // fileOption -- 1: save pkt file, otherwise save nothing.
 
 	/* disp familiy */
-	char				errbuf[PCAP_ERRBUF_SIZE];
-	int					i;			// for general use
-	int					ndNum=0;	// number of network devices
-	int					devNum;		// device Id used for online packet capture
-        pcap_dumper_t       *pd;        // dump file pointer
+	char errbuf[PCAP_ERRBUF_SIZE];
+	int i;		// for general use
+	int ndNum=0;	// number of network devices
+	int devNum;	// device Id used for online packet capture
+        
+	pcap_dumper_t *pd;  // dump file pointer
 
-	cpkNum = 0;
+	cpkNum = 0;  // Initialize global variable
 
 	switch(argc)
 	{
-		printf("# ARGC : %d \n", argc);
 		case 1: // case of none-option
 			maxPkt = MAXPKT;
 			break;
-		case 3: // case of write option
-			if(!strcmp(argv[1], "-f")) 
-			{
+		case 3: // case of unique option
+			if(!strcmp(argv[1], "-f")) {
 				strcpy(pktFileName, argv[2]);
 				fileOption = true;
-			}else if(!strcmp(argv[1], "-t"))
-			{
+			}else if(!strcmp(argv[1], "-t")) {
 				maxPkt = atoi(argv[2]);
-			}else{
-				fprintf(stderr, "option error \n");
-				printf("%s [-f] [pktFileName.pkt] [-t] [number of max packet]\n", argv[0]);
-				perror("");
-				return -1;
+			}else {
+				DieWithError("there is no such option! check it again please.\n")
 			}
 			break;
-		case 5:
-			if(!strcmp(argv[1], "-f") && !strcmp(argv[3], "-t")) 
-			{
+		case 5: // case of two options
+			if(!strcmp(argv[1], "-f") && !strcmp(argv[3], "-t")) {
 				strcpy(pktFileName, argv[2]);
 				fileOption = true;
 				maxPkt = atoi(argv[4]);
-			}else if(!strcmp(argv[1], "-t") && !strcmp(argv[3], "-f"))
-			{
+			}else if(!strcmp(argv[1], "-t") && !strcmp(argv[3], "-f")) {
 				strcpy(pktFileName, argv[4]);
 				fileOption = true;
 				maxPkt = atoi(argv[2]);
-			}else{
-				fprintf(stderr, "Wrong Option!\n");
-				printf("%s [pktFileName.pkt]\n", argv[0]);
-				perror("");
-				return -1;
+			}else {
+				DieWithError("there is no such option! check it again please.\n")
 			}
 			break;
 		default:
-			fprintf(stderr, "Wrong Argument!\n");
-			printf("%s [-f] [pktFileName.pkt] [-t] [number of max packet]\n", argv[0]);
-			return -1;
+			DieWithError("there is no such option! check it again please.\n")
 	}
 
     /* Retrieve the device list */
@@ -85,7 +80,7 @@ int main(int argc, char** argv)
     }
     
     /* select device for online packet capture application */
-    printf("Enter the interface number (1-%d):",ndNum);
+    printf("Enter the interface number (1-%d):", ndNum);
     scanf("%d", &devNum);
     
     /* select error ? */
@@ -119,6 +114,7 @@ int main(int argc, char** argv)
     /* At this point, we don't need any more the device list. Free it */
     pcap_freealldevs(alldevs);
     pd = NULL;
+
     if(fileOption==true)
     {
 	pd = pcap_dump_open(adhandle, pktFileName);
